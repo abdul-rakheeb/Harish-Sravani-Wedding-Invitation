465f02f78ca04902f6da09b83e938a062d5ecef9
abdul-rakheeb <mdabdulrakheeb814@gmail.com>
2026-07-16 11:31:40 +0530

    updated for scroll

--- script.js
+++ script.js
@@ -95,4 +95,23 @@ lightbox.addEventListener("click", (e) => {
         lightbox.style.display = "none";
     }
 });
+// ===========================
+// Scroll Animation
+// ===========================
 
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+},{
+    threshold:0.2
+});
+
+sections.forEach((section)=>{
+    section.classList.add("hidden");
+    observer.observe(section);
+});
